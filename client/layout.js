d332ce5a655e092bcc648871b73ef19b97cee290
bobbigmac <admin@bobbigmac.com>
2015-05-03 20:55:23 +0100

    Using FlexScrollView and WheelLayout for a bit more flavour

--- client/layout.js
+++ client/layout.js
@@ -2,7 +2,10 @@ Template.layout.helpers({
 	//See: https://github.com/gadicc/fview-flex/
 	'layoutOptions': function() {
 		return {
-  			margins: [70, 70, 70, 70]
+  			//margins: [10, 10, 10, 10],
+  			itemSize: 40,//[90,90],
+  			diameter: 400,
+  			radialOpacity: 1
 		};
 	}
 });
